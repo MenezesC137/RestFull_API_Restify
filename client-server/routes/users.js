64d9f799bcad6c80a1e195ddb4f36888755c394b
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-11 10:30:03 -0300

    Post and Put

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -14,7 +14,54 @@ router.get('/', function (req, res, next) {
   client.get('/users', function (err, request, response, obj) {
     assert.ifError(err)
 
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
+
+  })
+
+})
+
+router.get('/:id', function (req, res, next) {
+
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err)
+
+    res.json(obj);
+
+  })
+
+})
+
+//Editar
+router.put('/:id', function (req, res, next) {
+
+  client.put(`/users/${req.params.id}`, req.body, function (err, request, response, obj) {
+    assert.ifError(err)
+
+    res.json(obj);
+
+  })
+
+})
+
+//Deletar
+router.delete('/:id', function (req, res, next) {
+
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err)
+
+    res.json(obj);
+
+  })
+
+})
+
+//Primeiro cadastro
+router.post('/', function (req, res, next) {
+
+  client.post(`/users`, req.body, function (err, request, response, obj) {
+    assert.ifError(err)
+
+    res.json(obj);
 
   })
 
